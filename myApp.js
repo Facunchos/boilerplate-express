66f32417c3af2886b3059afc6c1371e32c90bcfd
Martinez, Facundo <fmartinez@merlindataquality.com>
2023-02-01 11:29:16 -0300

    All Ex completed

--- myApp.js
+++ myApp.js
@@ -2,34 +2,36 @@
 const { json } = require('body-parser');
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser')
 require('dotenv').config()
 // console.log("Hello World");
 // app.listen(3000)
 // let absolutePath = __dirname + path
-// let absolute = (path)=>{
-//     let absolutePath = __dirname + path
-//     return absolutePath
-// }
+
+let absolute = (path)=>{
+    let absolutePath = __dirname + path
+    return absolutePath
+}
 app.get('/', (req, res) => {
     res.sendFile(absolute("/views/index.html"))
 });
-// app.use("/public", express.static(absolute("/public")))
+app.use("/public", express.static(absolute("/public")))
 
-// app.get("/json", (req, res) => {
-//     let style = process.env.MESSAGE_STYLE
-//     let message = "Hello json"
-//     // let message = "prueba";
-//     if (style == 'uppercase') {
-//         message = message.toUpperCase();
-//     }
-//     res.json({"message":message})
-// })
+app.get("/json", (req, res) => {
+    let style = process.env.MESSAGE_STYLE
+    let message = "Hello json"
+    // let message = "prueba";
+    if (style == 'uppercase') {
+        message = message.toUpperCase();
+    }
+    res.json({"message":message})
+})
+
 app.use((req,res,next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
     // express.static(req.method + " " + req.path + " - " + reqs.ip);
     next();
-}
-)
+})
 
 
 app.get("/now", (req,res,next) => {
@@ -43,15 +45,17 @@ app.get(`/:word/echo`,(req, res, next) => {
     res.json({echo: req.params.word})
 })
 
-
+app.use("", bodyParser.urlencoded({extended: false}))
 
 app.route('/name').get((req, res, next) => {
-
-    res.json({ name: `${req.query.first} ${req.query.last}` })
+    let fName = req.body.first, lName = req.body.last;
+    res.json({ name: `${fName} ${lName}` })
     next();
+}).post((req, res) => {
+    let fName = req.body.first, lName = req.body.last;
+
+    res.json({ name: `${fName} ${lName}` })
 })
-
-
 
 
 
